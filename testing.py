5d63a52c7438353d63ff71af3a1d34bb6ffb48a6
Worakrit Kullanatpokin <130132510+uzimpp@users.noreply.github.com>
2023-06-08 20:12:44 +0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,15 +1,16 @@
+# To test the code. Basically, run the programm by include these codes and use ngrok to connect a webhook with LINE Official Account
 from pyngrok import ngrok
 from flask import Flask, request, abort
 
 app = Flask(__name__)
-http_tunnel = ngrok.connect(5000) #your port
+http_tunnel = ngrok.connect(5000) # A numbers in (5000) is your port
 
-@app.route("/callback", methods=['POST']) #replace def lambda_handler with this part instead
+# Replace def lambda_handler with this part instead
+@app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers['X-Line-Signature'] # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body) # get request body as text
-    # handle webhook body
+    app.logger.info("Request body: " + body)
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -18,3 +19,7 @@ def callback():
         )
         abort(400)
     return 'OK'
+
+# Put this part underneath the code
+if __name__ == "__main__":
+    app.run()
